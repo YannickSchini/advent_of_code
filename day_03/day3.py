d876a3a2064f096e20b08c9f401cb286157c8616
Yannick SCHINI <yannick.schini@octo.com>
2020-12-03 09:16:34 +0100

    feat(day3): What kind of terrain is at (x,y) coordinates
    I used the modulo operator to perform the "wrap around" thing where the
    same terrain gets transposed over and over to the right.

--- day_03/day3.py
+++ day_03/day3.py
@@ -7,8 +7,23 @@ class localGeology():
             list_of_lines = [x.strip() for x in list_of_lines]
         self.terrain = list_of_lines
 
+    def _get_nature_of_the_spot(self, x_coord, y_coord):
+        """ x : horizontal axis towards the right, 0 is in top left
+            y : vertical axis towards the bottom, 0 is in top left"""
+        if y_coord > len(self.terrain):
+            raise OutOfBoundsError
+        return self.terrain[y_coord][x_coord % len(self.terrain[0])]
+
+    def
+
     def test(self):
         print(self.terrain)
+        print(self.get_nature_of_the_spot(3, 1))
+        print(self.get_nature_of_the_spot(6, 2))
+        print(self.get_nature_of_the_spot(9, 3))
+        print(self.get_nature_of_the_spot(12, 4))
+        print(self.get_nature_of_the_spot(15, 5))
+        # print(self._get_nature_of_the_spot(15, 15))
 
 
 if __name__=="__main__":
