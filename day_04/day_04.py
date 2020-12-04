65a660d517498b7707d7697c2a43771a78db7232
Yannick SCHINI <yannick.schini@octo.com>
2020-12-04 11:26:25 +0100

    feat(day4): Part 2

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -24,17 +24,81 @@ class passport_validator():
         return passport_list
 
     def validate_passports(self):
-        validation_list = self._check_all_mandatory_keys_are_present()
-        return validation_list
-
-    def _check_all_mandatory_keys_are_present(self):
         validation_list = []
         for passport in self.passport_list:
-            if self.mandatory_keys.issubset(set(passport.keys())):
+            if self._check_all_mandatory_keys_are_present(passport) and \
+            self._check_if_byr_is_valid(passport["byr"]) and \
+            self._check_if_iyr_is_valid(passport["iyr"]) and \
+            self._check_if_eyr_is_valid(passport["eyr"]) and \
+            self._check_if_hgt_is_valid(passport["hgt"]) and \
+            self._check_if_hcl_is_valid(passport["hcl"]) and \
+            self._check_if_ecl_is_valid(passport["ecl"]) and \
+            self._check_if_pid_is_valid(passport["pid"]):
                 validation_list.append(True)
             else:
                 validation_list.append(False)
         return validation_list
+
+    def _check_all_mandatory_keys_are_present(self, passport):
+        if self.mandatory_keys.issubset(set(passport.keys())):
+            return True
+        else:
+            return False
+
+    def _check_if_byr_is_valid(self, byr_value):
+        try:
+            byr = int(byr_value)
+            if 1920 <= byr <= 2002:
+                return True
+        except ValueError:
+            pass
+        return False
+
+    def _check_if_iyr_is_valid(self, iyr_value):
+        try:
+            iyr = int(iyr_value)
+            if 2010 <= iyr <= 2020:
+                return True
+        except ValueError:
+            pass
+        return False
+
+    def _check_if_eyr_is_valid(self, eyr_value):
+        try:
+            eyr = int(eyr_value)
+            if 2020 <= eyr <= 2030:
+                return True
+        except ValueError:
+            pass
+        return False
+
+    def _check_if_hgt_is_valid(self, hgt_value):
+        hgt_unit = hgt_value[-2:]
+        hgt_numerical = hgt_value[:-2]
+        try:
+            hgt = int(hgt_numerical)
+        except ValueError:
+            return False
+        if hgt_unit == "in":
+            if 59 <= hgt <= 76:
+                return True
+        if hgt_unit == "cm":
+            if 150 <= hgt <= 193:
+                return True
+        return False
+
+    def _check_if_hcl_is_valid(self, hcl_value):
+        if re.match("^#[0-9a-f]{6}", hcl_value) is not None:
+            return True
+        return False
+
+    def _check_if_ecl_is_valid(self, ecl_value):
+        return (ecl_value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
+
+    def _check_if_pid_is_valid(self, pid_value):
+        if re.match("^[0-9]{9}$", pid_value) is not None:
+            return True
+        return False
 
 
 if __name__ == "__main__":
--- day_04/test_day_04.py
+++ day_04/test_day_04.py
@@ -69,6 +69,99 @@ class PassportValidatorTest(unittest.TestCase):
         # Then
         self.assertEqual(sum(validations), 2)
 
+    def test_check_if_byr_is_valid(self):
+        # Given
+        pass_val = passport_validator('test_file.txt')
+        valid_byr = 2002
+        invalid_byr = 2003
+        # When
+        valid_result = pass_val._check_if_byr_is_valid(valid_byr)
+        invalid_result = pass_val._check_if_byr_is_valid(invalid_byr)
+        # Then
+        self.assertTrue(valid_result)
+        self.assertFalse(invalid_result)
+
+    def test_check_if_iyr_is_valid(self):
+        # Given
+        pass_val = passport_validator('test_file.txt')
+        valid_iyr = 2012
+        invalid_iyr = 2023
+        # When
+        valid_result = pass_val._check_if_iyr_is_valid(valid_iyr)
+        invalid_result = pass_val._check_if_iyr_is_valid(invalid_iyr)
+        # Then
+        self.assertTrue(valid_result)
+        self.assertFalse(invalid_result)
+
+    def test_check_if_eyr_is_valid(self):
+        # Given
+        pass_val = passport_validator('test_file.txt')
+        valid_eyr = 2022
+        invalid_eyr = 2013
+        # When
+        valid_result = pass_val._check_if_eyr_is_valid(valid_eyr)
+        invalid_result = pass_val._check_if_eyr_is_valid(invalid_eyr)
+        # Then
+        self.assertTrue(valid_result)
+        self.assertFalse(invalid_result)
+
+    def test_check_if_hgt_is_valid(self):
+        # Given
+        pass_val = passport_validator('test_file.txt')
+        valid_hgt_1 = "60in"
+        valid_hgt_2 = "190cm"
+        invalid_hgt_1 = "190in"
+        invalid_hgt_2 = 190
+        # When
+        valid_result_1 = pass_val._check_if_hgt_is_valid(valid_hgt_1)
+        valid_result_2 = pass_val._check_if_hgt_is_valid(valid_hgt_2)
+        invalid_result_1 = pass_val._check_if_eyr_is_valid(invalid_hgt_1)
+        invalid_result_2 = pass_val._check_if_eyr_is_valid(invalid_hgt_2)
+        # Then
+        self.assertTrue(valid_result_1)
+        self.assertTrue(valid_result_2)
+        self.assertFalse(invalid_result_1)
+        self.assertFalse(invalid_result_2)
+
+    def test_check_if_hcl_is_valid(self):
+        # Given
+        pass_val = passport_validator('test_file.txt')
+        valid_hcl = '#123abc'
+        invalid_hcl_1 = '#123abz'
+        invalid_hcl_2 = '123abc'
+        # When
+        valid_result = pass_val._check_if_hcl_is_valid(valid_hcl)
+        invalid_result_1 = pass_val._check_if_hcl_is_valid(invalid_hcl_1)
+        invalid_result_2 = pass_val._check_if_hcl_is_valid(invalid_hcl_2)
+        # Then
+        self.assertTrue(valid_result)
+        self.assertFalse(invalid_result_1)
+        self.assertFalse(invalid_result_2)
+
+    def test_check_if_ecl_is_valid(self):
+        # Given
+        pass_val = passport_validator('test_file.txt')
+        valid_ecl = 'brn'
+        invalid_ecl = 'wat'
+        # When
+        valid_result = pass_val._check_if_ecl_is_valid(valid_ecl)
+        invalid_result = pass_val._check_if_ecl_is_valid(invalid_ecl)
+        # Then
+        self.assertTrue(valid_result)
+        self.assertFalse(invalid_result)
+
+    def test_check_if_pid_is_valid(self):
+        # Given
+        pass_val = passport_validator('test_file.txt')
+        valid_pid = '000000001'
+        invalid_pid = '0123456789'
+        # When
+        valid_result = pass_val._check_if_pid_is_valid(valid_pid)
+        invalid_result = pass_val._check_if_pid_is_valid(invalid_pid)
+        # Then
+        self.assertTrue(valid_result)
+        self.assertFalse(invalid_result)
+
 
 if __name__ == "__main__":
     unittest.main()
