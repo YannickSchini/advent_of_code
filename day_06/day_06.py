23059408e5c8b604770f9828b3b4ee939a96ea16
Yannick SCHINI <yannick.schini@octo.com>
2020-12-06 10:15:45 +0100

    feat(day_06): part 2 !

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -6,5 +6,20 @@ def part_1(file_path):
         [len(set(x.replace('\n', ''))) for x in unprocessed_customs_list])
 
 
+def part_2(file_path):
+    with open(file_path, "r") as customs_file:
+        unprocessed_customs_string = customs_file.read()
+    unprocessed_customs_list = unprocessed_customs_string.split('\n\n')
+    intermediate_list = [x.split('\n') for x in unprocessed_customs_list]
+    intermediate_list_2 = [[set(y) for y in x if y != '']
+                           for x in intermediate_list]
+    intermediate_list_3 = [
+        len(set.intersection(*x)) for x in intermediate_list_2
+    ]
+    print(intermediate_list_3)
+    return sum(intermediate_list_3)
+
+
 if __name__ == "__main__":
     print(part_1('input_file.txt'))
+    print(part_2('input_file.txt'))
