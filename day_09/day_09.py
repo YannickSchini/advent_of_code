f88d7c6dffabdd53ff9de245bbdf2676ad7b7464
Yannick SCHINI <yannick.schini@octo.com>
2020-12-09 10:10:29 +0100

    feat(day_09): part 2

--- day_09/day_09.py
+++ day_09/day_09.py
@@ -5,10 +5,7 @@ def can_number_be_decomposed(number: int, number_list: list) -> bool:
     return False
 
 
-def process_xmas_file(file_path: str, PREAMBLE_SIZE: int) -> int:
-    with open(file_path, "r") as xmas_input:
-        number_list = xmas_input.readlines()
-        number_list = [int(x) for x in number_list]
+def process_xmas_file(number_list: list, PREAMBLE_SIZE: int) -> int:
     for index in range(PREAMBLE_SIZE, len(number_list)):
         if can_number_be_decomposed(number_list[index],
                                     number_list[index - PREAMBLE_SIZE:index]):
@@ -18,5 +15,28 @@ def process_xmas_file(file_path: str, PREAMBLE_SIZE: int) -> int:
     return ValueError
 
 
+def find_sublist_that_sums_to_number(number: int, number_list: list) -> list:
+    for index in range(len(number_list)):
+        sublist_sum = number_list[index]
+        sublist_len = 1
+        while sublist_sum < number:
+            sublist_sum += number_list[index + sublist_len]
+            sublist_len += 1
+        if sublist_sum == number:
+            return number_list[index:index + sublist_len]
+        else:
+            pass
+    return ValueError
+
+
 if __name__ == "__main__":
-    print("Part 1:", process_xmas_file("input_file.txt", 25))
+    file_path = "input_file.txt"
+    with open(file_path, "r") as xmas_input:
+        number_list = xmas_input.readlines()
+        number_list = [int(x) for x in number_list]
+    number_with_property = process_xmas_file(number_list, 25)
+    print("Part 1:", number_with_property)
+    sublist = find_sublist_that_sums_to_number(number_with_property,
+                                               number_list)
+    print(sublist)
+    print("Part 2:", min(sublist) + max(sublist))
--- day_09/test_day_09.py
+++ day_09/test_day_09.py
@@ -19,10 +19,31 @@ class testDay08(unittest.TestCase):
     def test_process_xmas_file(self):
         # Given
         file_path = "test_file.txt"
+        with open(file_path, "r") as xmas_input:
+            number_list = xmas_input.readlines()
+            number_list = [int(x) for x in number_list]
         # When
-        result = day_09.process_xmas_file(file_path=file_path, PREAMBLE_SIZE=5)
+        result = day_09.process_xmas_file(number_list=number_list,
+                                          PREAMBLE_SIZE=5)
         # Then
         self.assertEqual(result, 127)
+
+    def test_find_sublist_that_sums_to_number(self):
+        # Given
+        number_1 = 35
+        number_list_1 = [10, 12, 13, 45, 50]
+        sublist_1 = [10, 12, 13]
+        number_2 = 85
+        number_list_2 = [10, 11, 13, 12, 40, 20, 100]
+        sublist_2 = [13, 12, 40, 20]
+        # When
+        result_1 = day_09.find_sublist_that_sums_to_number(
+            number_1, number_list_1)
+        result_2 = day_09.find_sublist_that_sums_to_number(
+            number_2, number_list_2)
+        # Then
+        self.assertEqual(result_1, sublist_1)
+        self.assertEqual(result_2, sublist_2)
 
 
 if __name__ == "__main__":
