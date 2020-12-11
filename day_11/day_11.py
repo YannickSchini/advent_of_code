ee5b4f5df1881ecaef746da43af42538f184d6d5
Yannick SCHINI <yannick.schini@octo.com>
2020-12-11 15:38:33 +0100

    feat(day_11): Part 1

---
+++ day_11/day_11.py
@@ -0,0 +1,66 @@
+from typing import List
+
+
+class seatingArea:
+    def __init__(self, file_path):
+        self.floor_plan: List[List[str]] = self._get_floor_plan_from_file(
+            file_path)
+        self.seats = self._get_seat_dict()
+        self.adjacent_seat_combinations = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
+                                           (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    @staticmethod
+    def _get_floor_plan_from_file(file_path):
+        with open(file_path, "r") as floor_plan_file:
+            return [x.strip('\n') for x in floor_plan_file.readlines()]
+
+    def _get_seat_dict(self):
+        seat_dict = {}
+        for row_number in range(len(self.floor_plan)):
+            for column_number in range(len(self.floor_plan[row_number])):
+                seat_dict[(row_number, column_number
+                           )] = self.floor_plan[row_number][column_number]
+        return seat_dict
+
+    def _get_seat_value(self, row_num, col_num):
+        if row_num < 0 or col_num < 0 or row_num >= len(
+                self.floor_plan) or col_num >= len(self.floor_plan[0]):
+            return IndexError
+        else:
+            return self.seats[(row_num, col_num)]
+
+    def update_seat_status(self, row_num, col_num):
+        current_seat_status = self.seats[(row_num, col_num)]
+        number_of_adjacent_occupied_seats = 0
+        for row_offset, col_offset in self.adjacent_seat_combinations:
+            new_seat_value = self._get_seat_value(row_num + row_offset,
+                                                  col_num + col_offset)
+            if new_seat_value == IndexError:
+                pass
+            elif new_seat_value == '#':
+                number_of_adjacent_occupied_seats += 1
+        if current_seat_status == "L" and number_of_adjacent_occupied_seats == 0:
+            return "#"
+        elif current_seat_status == "#" and number_of_adjacent_occupied_seats >= 4:
+            return "L"
+        else:
+            return current_seat_status
+
+    def update_seating_status(self):
+        new_seat_dict = self.seats.copy()
+        for row_num, col_num in self.seats.keys():
+            new_seat_dict[(row_num, col_num)] = self.update_seat_status(
+                row_num, col_num)
+        return new_seat_dict
+
+    def get_stabilised_occupied_seat_count(self):
+        new_seat_dict = self.update_seating_status()
+        while new_seat_dict != self.seats:
+            self.seats = new_seat_dict
+            new_seat_dict = self.update_seating_status()
+        return sum([(seat_value == "#") for seat_value in self.seats.values()])
+
+
+if __name__ == "__main__":
+    boarding_area = seatingArea("input_file.txt")
+    print("Part 1: ", boarding_area.get_stabilised_occupied_seat_count())
---
+++ day_11/test_day_11.py
@@ -0,0 +1,72 @@
+import unittest
+from day_11 import seatingArea
+
+
+class testSeatingArea(unittest.TestCase):
+    def test_floor_plan_is_read_correctly(self):
+        # Given
+        expected_floor_plan = [
+            'L.LL.LL.LL', 'LLLLLLL.LL', 'L.L.L..L..', 'LLLL.LL.LL',
+            'L.LL.LL.LL', 'L.LLLLL.LL', '..L.L.....', 'LLLLLLLLLL',
+            'L.LLLLLL.L', 'L.LLLLL.LL'
+        ]
+        # When
+        testArea = seatingArea("test_file.txt")
+        # Then
+        self.assertEqual(testArea.floor_plan, expected_floor_plan)
+
+    def test_get_seat_value(self):
+        # Given
+        testArea = seatingArea("test_file.txt")
+        empty_seat_coord = (0, 0)
+        floor_coord = (2, 1)
+        invalid_coord_1 = (-1, 2)
+        invalid_coord_2 = (8, 11)
+        # When
+        empty_seat_value = testArea._get_seat_value(*empty_seat_coord)
+        floor_value = testArea._get_seat_value(*floor_coord)
+        error_value_1 = testArea._get_seat_value(*invalid_coord_1)
+        error_value_2 = testArea._get_seat_value(*invalid_coord_2)
+        # Then
+        self.assertEqual(empty_seat_value, 'L')
+        self.assertEqual(floor_value, '.')
+        self.assertEqual(error_value_1, IndexError)
+        self.assertEqual(error_value_2, IndexError)
+
+    def test_update_seating_status_gives_correct_results(self):
+        # Given
+        testArea = seatingArea("test_file.txt")
+        after_round_1 = seatingArea("after_round_1.txt")
+        after_round_2 = seatingArea("after_round_2.txt")
+        after_round_3 = seatingArea("after_round_3.txt")
+        after_round_4 = seatingArea("after_round_4.txt")
+        after_round_5 = seatingArea("after_round_5.txt")
+        # When
+        result_after_1_round = testArea.update_seating_status()
+        testArea.seats = result_after_1_round
+        result_after_2_round = testArea.update_seating_status()
+        testArea.seats = result_after_2_round
+        result_after_3_round = testArea.update_seating_status()
+        testArea.seats = result_after_3_round
+        result_after_4_round = testArea.update_seating_status()
+        testArea.seats = result_after_4_round
+        result_after_5_round = testArea.update_seating_status()
+        testArea.seats = result_after_5_round
+        # Then
+        self.assertEqual(result_after_1_round, after_round_1.seats)
+        self.assertEqual(result_after_2_round, after_round_2.seats)
+        self.assertEqual(result_after_3_round, after_round_3.seats)
+        self.assertEqual(result_after_4_round, after_round_4.seats)
+        self.assertEqual(result_after_5_round, after_round_5.seats)
+
+    def test_get_stabilised_occupied_seat_count_get_correct_value(self):
+        # Given
+        testArea = seatingArea("test_file.txt")
+        # When
+        result = testArea.get_stabilised_occupied_seat_count()
+        # Then
+        self.assertEqual(result, 37)
+
+
+if __name__ == "__main__":
+    unittest.main()
