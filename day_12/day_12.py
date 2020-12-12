e69d6b11ac4f1d870a33699cf05e7c063eaf217e
Yannick SCHINI <yannick.schini@octo.com>
2020-12-12 12:53:43 +0100

    feat(day_12): part 2

--- day_12/day_12.py
+++ day_12/day_12.py
@@ -1,4 +1,4 @@
-class Ship:
+class Ship_part_1:
     def __init__(self, file_path):
         self.coord = (0, 0)
         self.direction = 90
@@ -45,7 +45,83 @@ class Ship:
         return abs(x_coord) + abs(y_coord)
 
 
+class Ship_part_2:
+    def __init__(self, file_path):
+        self.coord = (0, 0)
+        self.waypoint_direction = (-10, 1)
+        self.instructions = self._load_instructions(file_path)
+
+    @staticmethod
+    def _load_instructions(file_path):
+        with open(file_path, "r") as instruction_file:
+            list_of_instruction_strings = instruction_file.readlines()
+            list_of_instruction_strings = [
+                x.rstrip('\n') for x in list_of_instruction_strings
+            ]
+            instructions = []
+            for instruction_string in list_of_instruction_strings:
+                instructions.append(
+                    (instruction_string[0], int(instruction_string[1:])))
+            return instructions
+
+    def _make_one_move(self, instruction):
+        print(instruction)
+        command, value = instruction
+        x_coord, y_coord = self.coord
+        x_direction, y_direction = self.waypoint_direction
+        if command == "F":
+            self.coord = (x_coord + value * x_direction,
+                          y_coord + value * y_direction)
+            x_coord, y_coord = self.coord
+        elif command == "N":
+            self.waypoint_direction = (x_direction, y_direction + value)
+        elif command == "S":
+            self.waypoint_direction = (x_direction, y_direction - value)
+        elif command == "E":
+            self.waypoint_direction = (x_direction - value, y_direction)
+        elif command == "W":
+            self.waypoint_direction = (x_direction + value, y_direction)
+        elif command == "L":
+            for i in range(value // 90):
+                self.waypoint_direction = self._turn_left(
+                    x_direction, y_direction)
+                x_direction, y_direction = self.waypoint_direction
+        elif command == "R":
+            for i in range(value // 90):
+                self.waypoint_direction = self._turn_right(
+                    x_direction, y_direction)
+                x_direction, y_direction = self.waypoint_direction
+        else:
+            raise ValueError
+        print(self.coord, self.waypoint_direction)
+
+    @staticmethod
+    def _turn_left(x_coord, y_coord):
+        return (y_coord, -1 * x_coord)
+
+    @staticmethod
+    def _turn_right(x_coord, y_coord):
+        return (-1 * y_coord, x_coord)
+
+    def _get_direction_to_waypoint(self):
+        x_coord, y_coord = self.coord
+        wp_x_coord, wp_y_coord = self.waypoint_direction
+        return (wp_x_coord - x_coord, wp_y_coord - y_coord)
+
+    def make_all_moves(self):
+        for instruction in self.instructions:
+            self._make_one_move(instruction)
+        return self.coord
+
+    def calculate_distance(self):
+        x_coord, y_coord = self.coord
+        return abs(x_coord) + abs(y_coord)
+
+
 if __name__ == "__main__":
-    test_ship = Ship("input_file.txt")
-    print(test_ship.make_all_moves())
-    print(test_ship.calculate_distance())
+    ship_part_1 = Ship_part_1("input_file.txt")
+    print(ship_part_1.make_all_moves())
+    print(ship_part_1.calculate_distance())
+    ship_part_2 = Ship_part_2("input_file.txt")
+    print(ship_part_2.make_all_moves())
+    print(ship_part_2.calculate_distance())
--- day_12/test_day_12.py
+++ day_12/test_day_12.py
@@ -5,7 +5,7 @@ import day_12
 class testDay12(unittest.TestCase):
     def test_part_1(self):
         # Given
-        testShip = day_12.Ship("test_file.txt")
+        testShip = day_12.Ship_part_1("test_file.txt")
         testShip.make_all_moves()
         # When
         distance = testShip.calculate_distance()
