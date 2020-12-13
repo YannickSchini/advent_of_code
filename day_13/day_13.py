2ceb488ec228387609395b0cc5e30ab056d7ed99
Yannick SCHINI <yannick.schini@octo.com>
2020-12-13 19:36:52 +0100

    feat(day_13): part 2, optimized !

--- day_13/day_13.py
+++ day_13/day_13.py
@@ -1,4 +1,5 @@
 import operator
+from numpy import prod
 
 
 def part_1(file_path):
@@ -23,18 +24,25 @@ def part_2(file_path):
         bus_ID_string = bus_times_file.readline()
     bus_ID_list = bus_ID_string.rstrip('\n').split(',')
 
-    timestamp = 0
+    timestamp = min([int(x) for x in bus_ID_list if x != "x"])
     continue_loop = True
+    max_counter = 0
+    increment = min([int(x) for x in bus_ID_list if x != "x"])
     while continue_loop:
-        if timestamp % 100000 == 0:
+        if timestamp % 1000000 == 0:
             print(timestamp)
-        timestamp += min([int(x) for x in bus_ID_list if x != "x"])
+        timestamp += increment
         counter = 0
         for ID in bus_ID_list:
             if does_bus_run_at_timestamp(ID, timestamp + counter):
                 counter += 1
                 continue
             else:
+                if counter > max_counter:
+                    max_counter = counter
+                    increment = prod([
+                        int(x) for x in bus_ID_list[:max_counter] if x != "x"
+                    ])
                 break
         if counter == len(bus_ID_list):
             continue_loop = False
