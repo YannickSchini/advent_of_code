c2bbe21da90f45b191db72d096e66a20d0d7826c
Yannick SCHINI <yannick.schini@octo.com>
2020-12-15 10:01:32 +0100

    chore(day_15): code for part 2, but not fast enough

--- day_15/day_15.py
+++ day_15/day_15.py
@@ -1,17 +1,17 @@
-def part_1(starting_number_list):
+def day_15_test(starting_number_list, number_to_go_to):
     number_list = starting_number_list
-    while len(number_list) < 2020:
-        if number_list[-1] not in number_list[:-1]:
+    while len(number_list) < number_to_go_to:
+        if len(number_list) % 100000 == 0:
+            print(starting_number_list)
+        try:
+            index = number_list[:-1][::-1].index(number_list[-1])
+            number_list.append(index + 1)
+        except ValueError:
             number_list.append(0)
-        else:
-            index_list = [
-                index for index in range(len(number_list) - 1)
-                if number_list[index] == number_list[-1]
-            ]
-            number_list.append(len(number_list) - max(index_list) - 1)
     return number_list[-1]
 
 
 if __name__ == "__main__":
     input_list = [1, 2, 16, 19, 18, 0]
-    print(part_1(input_list))
+    print("Part 1: ", day_15_test(input_list, 2020))
+    print("Part 2: ", day_15_test(input_list, 30000000))
--- day_15/test_day_15.py
+++ day_15/test_day_15.py
@@ -13,13 +13,39 @@ class testDay15(unittest.TestCase):
         starting_6 = [3, 2, 1]
         starting_7 = [3, 1, 2]
         # When
-        result_1 = day_15.part_1(starting_1)
-        result_2 = day_15.part_1(starting_2)
-        result_3 = day_15.part_1(starting_3)
-        result_4 = day_15.part_1(starting_4)
-        result_5 = day_15.part_1(starting_5)
-        result_6 = day_15.part_1(starting_6)
-        result_7 = day_15.part_1(starting_7)
+        result_1 = day_15.day_15_test(starting_1, 2020)
+        result_2 = day_15.day_15_test(starting_2, 2020)
+        result_3 = day_15.day_15_test(starting_3, 2020)
+        result_4 = day_15.day_15_test(starting_4, 2020)
+        result_5 = day_15.day_15_test(starting_5, 2020)
+        result_6 = day_15.day_15_test(starting_6, 2020)
+        result_7 = day_15.day_15_test(starting_7, 2020)
+        # Then
+        self.assertEqual(result_1, 436)
+        self.assertEqual(result_2, 1)
+        self.assertEqual(result_3, 10)
+        self.assertEqual(result_4, 27)
+        self.assertEqual(result_5, 78)
+        self.assertEqual(result_6, 438)
+        self.assertEqual(result_7, 1836)
+
+    def test_part_2(self):
+        # Given
+        starting_1 = [0, 3, 6]
+        starting_2 = [1, 3, 2]
+        starting_3 = [2, 1, 3]
+        starting_4 = [1, 2, 3]
+        starting_5 = [2, 3, 1]
+        starting_6 = [3, 2, 1]
+        starting_7 = [3, 1, 2]
+        # When
+        result_1 = day_15.day_15_test(starting_1, 30000000)
+        result_2 = day_15.day_15_test(starting_2, 30000000)
+        result_3 = day_15.day_15_test(starting_3, 30000000)
+        result_4 = day_15.day_15_test(starting_4, 30000000)
+        result_5 = day_15.day_15_test(starting_5, 30000000)
+        result_6 = day_15.day_15_test(starting_6, 30000000)
+        result_7 = day_15.day_15_test(starting_7, 30000000)
         # Then
         self.assertEqual(result_1, 436)
         self.assertEqual(result_2, 1)
