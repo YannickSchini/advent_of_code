dabe708c7bf50984168e2a417d0c03eb9b9a3d60
Yannick SCHINI <yannick.schini@octo.com>
2020-12-24 15:56:07 +0100

    fix(day_23): Part 1 fixed !

--- day_23/day_23.py
+++ day_23/day_23.py
@@ -1,29 +1,34 @@
 from typing import Tuple, List
 
 
-def part_1(input_cup_string: str) -> str:
+def part_1(input_cup_string: str, number_of_games: int) -> str:
     cup_list = [int(x) for x in list(input_cup_string)]
     current_cup_index = 0
     current_cup = cup_list[current_cup_index]
-    for _ in range(100):
-        print("Cup list before playing: ", cup_list)
+    for game_number in range(number_of_games):
         cup_list = play_one_round(current_cup, current_cup_index, cup_list)
         current_cup_index = cup_list.index(current_cup) + 1
+        current_cup_index = current_cup_index % len(cup_list)
         current_cup = cup_list[current_cup_index % len(cup_list)]
-    return cup_list
+    return get_result_from_cuplist(cup_list)
+
+
+def get_result_from_cuplist(cup_list: List[int]) -> str:
+    index_of_1 = cup_list.index(1)
+    result_list = cup_list[index_of_1 + 1:] + cup_list[:index_of_1]
+    return ''.join([str(x) for x in result_list])
 
 
 def play_one_round(current_cup: int, current_cup_index: int,
                    cup_list: List[int]) -> List[int]:
     removed_cups = []
     for _ in range(3):
-        removed_cups.append(
-            cup_list.pop((current_cup_index + 1) % len(cup_list)))
-    print("Current cup: ", current_cup)
-    print("Removed cups: ", removed_cups)
+        try:
+            removed_cups.append(cup_list.pop(current_cup_index + 1))
+        except IndexError:
+            removed_cups.append(cup_list.pop(0))
     destination_cup_index, destination_cup = get_destination_cup(
         current_cup_index, current_cup, cup_list)
-    print("Destination cup: ", destination_cup)
     cup_list = cup_list[:destination_cup_index + 1] + \
         removed_cups + cup_list[destination_cup_index + 1:]
     return cup_list
@@ -44,4 +49,4 @@ def get_destination_cup(current_cup_index: int, current_cup: int,
 
 
 if __name__ == "__main__":
-    print(part_1("398254716"))
+    print(part_1("398254716", 100))
--- day_23/test_day_23.py
+++ day_23/test_day_23.py
@@ -7,9 +7,11 @@ class testDay23(unittest.TestCase):
         # Given
         input_cup_labeling = "389125467"
         # When
-        result = day_23.part_1(input_cup_labeling)
+        result_after_10 = day_23.part_1(input_cup_labeling, 10)
+        result_after_100 = day_23.part_1(input_cup_labeling, 100)
         # Then
-        self.assertEqual(result, "67384529")
+        self.assertEqual(result_after_10, "92658374")
+        self.assertEqual(result_after_100, "67384529")
 
 
 if __name__ == "__main__":
